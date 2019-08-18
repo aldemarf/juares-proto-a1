7e3fd0e8a78350a6d4bb9c05fedc21bb219624b3
Victor Medeiros <victor@jualabs.com>
2019-08-17 23:12:24 -0300

    improving code modularization - created SDUtil class

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,19 @@
 #include <Arduino.h>
 
-/*
- * Connect the SD card to the following pins:
- *
- * SD Card | ESP32
- *    D2       -
- *    D3       SS
- *    CMD      MOSI
- *    VSS      GND
- *    VDD      3.3V
- *    CLK      SCK
- *    VSS      GND
- *    D0       MISO
- *    D1       -
- */
-#include <FS.h>
-#include <SD.h>
-#include <SPI.h>
 #include <TinyGPS++.h>
 #include <TimeLib.h>
 #include <jled.h>
 #include <Wire.h>
 #include <WiFi.h>
+#include "SDUtil.h"
 
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 
 #define DEBUG
+
+// SD card control object
+SDUtil *sd = SDUtil::getInstance();
 
 // MPU-6050 constants
 static const uint8_t INTERRUPT_PIN = 2;
@@ -41,15 +28,6 @@ static const uint32_t GPSBaud = 9600;
 // GPS related variables
 TinyGPSPlus gps;
 HardwareSerial GPSSerial(1);
-
-// SD card constants
-static const uint8_t SCLK_PIN = 25; 
-static const uint8_t MISO_PIN = 32;
-static const uint8_t MOSI_PIN = 13;
-static const uint8_t SS_PIN = 33;
-
-// SD card related variables
-SPIClass *hspi = NULL;
 
 // sleep mode constants
 static const uint32_t  uS_TO_S_FACTOR = 1000;  /* Conversion factor for micro seconds to seconds */
@@ -112,209 +90,6 @@ static void smartDelay(unsigned long ms)
     } while (millis() - start < ms);
 }
 
-void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
-{
-    Serial.printf("Listing directory: %s\n", dirname);
-
-    File root = fs.open(dirname);
-    if (!root)
-    {
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if (!root.isDirectory())
-    {
-        Serial.println("Not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while (file)
-    {
-        if (file.isDirectory())
-        {
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if (levels)
-            {
-                listDir(fs, file.name(), levels - 1);
-            }
-        }
-        else
-        {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("  SIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
-
-void createDir(fs::FS &fs, const char *path)
-{
-    Serial.printf("Creating Dir: %s\n", path);
-    if (fs.mkdir(path))
-    {
-        Serial.println("Dir created");
-    }
-    else
-    {
-        Serial.println("mkdir failed");
-    }
-}
-
-void removeDir(fs::FS &fs, const char *path)
-{
-    Serial.printf("Removing Dir: %s\n", path);
-    if (fs.rmdir(path))
-    {
-        Serial.println("Dir removed");
-    }
-    else
-    {
-        Serial.println("rmdir failed");
-    }
-}
-
-void readFile(fs::FS &fs, const char *path)
-{
-    Serial.printf("Reading file: %s\n", path);
-
-    File file = fs.open(path);
-    if (!file)
-    {
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-
-    Serial.print("Read from file: ");
-    while (file.available())
-    {
-        Serial.write(file.read());
-    }
-    file.close();
-}
-
-void writeFile(fs::FS &fs, const char *path, const char *message)
-{
-    Serial.printf("Writing file: %s\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if (!file)
-    {
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if (file.print(message))
-    {
-        Serial.println("File written");
-    }
-    else
-    {
-        Serial.println("Write failed");
-    }
-    file.close();
-}
-
-void appendFile(fs::FS &fs, const char *path, const char *message)
-{
-    // Serial.printf("Appending to file: %s\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if (!file)
-    {
-        Serial.println("Failed to open file for appending");
-        status_led.Blink(100, 100).Forever();
-        return;
-    }
-    if (file.print(message))
-    {
-        // Serial.println("Message appended");
-    }
-    else
-    {
-        status_led.Blink(100, 100).Forever();
-        Serial.println("Append failed");
-    }
-    file.close();
-}
-
-void renameFile(fs::FS &fs, const char *path1, const char *path2)
-{
-    Serial.printf("Renaming file %s to %s\n", path1, path2);
-    if (fs.rename(path1, path2))
-    {
-        Serial.println("File renamed");
-    }
-    else
-    {
-        Serial.println("Rename failed");
-    }
-}
-
-void deleteFile(fs::FS &fs, const char *path)
-{
-    Serial.printf("Deleting file: %s\n", path);
-    if (fs.remove(path))
-    {
-        Serial.println("File deleted");
-    }
-    else
-    {
-        Serial.println("Delete failed");
-    }
-}
-
-void testFileIO(fs::FS &fs, const char *path)
-{
-    File file = fs.open(path);
-    static uint8_t buf[512];
-    size_t len = 0;
-    uint32_t start = millis();
-    uint32_t end = start;
-    if (file)
-    {
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        while (len)
-        {
-            size_t toRead = len;
-            if (toRead > 512)
-            {
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            len -= toRead;
-        }
-        end = millis() - start;
-        Serial.printf("%u bytes read for %u ms\n", flen, end);
-        file.close();
-    }
-    else
-    {
-        Serial.println("Failed to open file for reading");
-    }
-
-    file = fs.open(path, FILE_WRITE);
-    if (!file)
-    {
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-
-    size_t i;
-    start = millis();
-    for (i = 0; i < 2048; i++)
-    {
-        file.write(buf, 512);
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-    file.close();
-}
-
 void read_gps() {
   smartDelay(1);
   if (gps.location.isUpdated() && gps.location.isValid()) {
@@ -333,7 +108,7 @@ void read_gps() {
     Serial.println(sample_str);
 #endif
     // write GPS data on file
-    appendFile(SD, filename, sample_str);
+    sd->appendFile(SD, filename, sample_str);
     status_led.Blink(250, 250).Repeat(2);
   }
   status_led.Blink(500, 500).Forever();
@@ -360,7 +135,7 @@ void write_mpu_data() {
             mpu_samples[i].gX, mpu_samples[i].gY, mpu_samples[i].gZ,
             mpu_samples[i].aX, mpu_samples[i].aY, mpu_samples[i].aZ);
     // escreve valor 'x' do acelerometro no arquivo
-    appendFile(SD, filename, sample_str);
+    sd->appendFile(SD, filename, sample_str);
 #ifdef DEBUG
     if(i == 0) Serial.println(mpu_samples[0].ts);
     // Serial.println(sample_str);
@@ -378,7 +153,6 @@ void read_mpu() {
   Serial.print(" | fc: "); Serial.println(fifo_count);
 #endif
   while(fifo_count >= packet_size) {
-    mpu.getFIFOBy=
     fifo_count -= packet_size;
     mpu_samples[cur_sample].ts = now();
     mpu.dmpGetQuaternion(&mpu_samples[cur_sample].q, fifo_buffer);
@@ -387,8 +161,8 @@ void read_mpu() {
     mpu_samples[cur_sample].gZ = (fifo_buffer[24] << 8) | fifo_buffer[25];
     mpu_samples[cur_sample].aX = (fifo_buffer[28] << 8) | fifo_buffer[29];
     mpu_samples[cur_sample].aY = (fifo_buffer[32] << 8) | fifo_buffer[33];
-    mpu_samples[cur_sample].aZ = (fifo_buffer[36] << 8) | fifo_buffer[37]
-    `cur_sample++;
+    mpu_samples[cur_sample].aZ = (fifo_buffer[36] << 8) | fifo_buffer[37];
+    cur_sample++;
     // if buffer full
     if (cur_sample == NUM_SAMPLES) {
       write_mpu_data();
@@ -470,24 +244,7 @@ void setup() {
     GPSSerial.begin(9600, SERIAL_8N1, 12, 15);
     // init MPU-6050
     mpu_setup();
-    // init SD card SPI interface
-    hspi = new SPIClass(HSPI);
-    pinMode(SS_PIN, OUTPUT); //HSPI SS
-    // SCLK = 25, MISO = 32, MOSI = 13, SS = 33
-    hspi->begin(SCLK_PIN, MISO_PIN, MOSI_PIN, SS_PIN); //SCLK, MISO, MOSI, SS
-    if (!SD.begin(SS_PIN, *hspi)) {
-        Serial.println("Card Mount Failed");
-        status_led.Blink(100, 100).Forever();
-        return;
-    }
-    uint8_t cardType = SD.cardType();
-
-    if (cardType == CARD_NONE)
-    {
-        status_led.Blink(100, 1000).Forever();
-        Serial.println("No SD card attached");
-        return;
-    }
+    sd->setup();
   if(rst_reason == ESP_RST_POWERON) {
     // wait for GPS fix
     status_led.Blink(250, 250).Forever();
